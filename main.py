2c5cb5278be44b242d44694b92a5482c8479a4eb
User <lambargini312@gmail.com>
2023-12-14 19:53:50 +0200

    initial commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ class Student:
         self.name=name
         self.gladness=50
         self.progress=0
+        self.money=100
         self.alive=True
     def to_study(self):
         print('time to study')
@@ -16,6 +17,12 @@ class Student:
         print('Rest time')
         self.progress-=0.1
         self.gladness+=5
+        self.money-=20
+    def to_work(self):
+        print('Work time')
+        self.money+=30
+        self.gladness-=3
+        self.progress+=0.025
     def is_alive(self):
         if self.progress<-0.5:
             print('Cast out')
@@ -26,19 +33,25 @@ class Student:
         elif self.progress>5:
             print('Passed externaly')
             self.alive=False
+        elif self.money<0:
+            print('bankrupt')
+            self.alive=False
     def end_of_day(self):
         print(f'Gladness - {self.gladness}')
         print(f'Progress - {round(self.progress)}')
+        print(f'Money - {round(self.money)}')
     def live(self,day):
         day='Day ' + str(day) + ' of ' + self.name + ' life'
         print(f'{day:=^50}')
-        life_cube=random.randint(1,3)
+        life_cube=random.randint(1,4)
         if life_cube==1:
             self.to_study()
         elif life_cube==2:
             self.to_sleep()
         elif life_cube==3:
             self.to_chill()
+        elif life_cube==4:
+            self.to_work()
         self.end_of_day()
         self.is_alive()
 mavpich=Student(name='mavpich')
